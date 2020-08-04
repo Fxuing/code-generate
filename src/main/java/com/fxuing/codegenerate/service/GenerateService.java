1a3db8f757a34c84f222339317d0f3f417c41117
houfx <hou_fx@9hjf.com>
2020-08-04 15:56:05 +0800

    代码规范

--- src/main/java/com/fxuing/codegenerate/service/GenerateService.java
+++ src/main/java/com/fxuing/codegenerate/service/GenerateService.java
@@ -10,15 +10,75 @@ import java.sql.SQLException;
  * @Description:
  */
 public interface GenerateService {
+    /**
+     * 配置
+     *
+     * @param config
+     * @Author: Hou_fx
+     * @Date: 2020.8.4 10:52
+     * @Description:
+     */
     void setConfig(GenerateConfig config);
-    /** 生成模型 */
+
+    /**
+     * 生成模型
+     *
+     * @param tableName
+     * @Author: Hou_fx
+     * @Date: 2020.8.4 10:52
+     * @Description:
+     * @throws SQLException SQL异常
+     */
     void generateModel(String tableName) throws SQLException;
-    /** 生成dao */
+
+    /**
+     * 生成dao
+     *
+     * @param tableName
+     * @Author: Hou_fx
+     * @Date: 2020.8.4 10:52
+     * @Description:
+     */
     void generateDao(String tableName);
-    /** 生成mapper */
+
+    /**
+     * 生成 xml 映射
+     *
+     * @param tableName
+     * @Author: Hou_fx
+     * @Date: 2020.8.4 10:53
+     * @Description:
+     * @throws SQLException SQL异常
+     */
     void generateMapper(String tableName) throws SQLException;
 
+    /**
+     * 生成service
+     *
+     * @param tableName
+     * @Author: Hou_fx
+     * @Date: 2020.8.4 10:53
+     * @Description:
+     */
     void generateService(String tableName);
+
+    /**
+     * 生成实现类
+     *
+     * @param tableName
+     * @Author: Hou_fx
+     * @Date: 2020.8.4 10:53
+     * @Description:
+     */
     void generateServiceImpl(String tableName);
+
+    /**
+     * 生成controller
+     *
+     * @param tableName
+     * @Author: Hou_fx
+     * @Date: 2020.8.4 10:53
+     * @Description:
+     */
     void generateController(String tableName);
 }
